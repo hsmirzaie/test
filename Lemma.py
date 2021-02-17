ce61970b9ab72e3796daa8a8d2fb10f8af5adcba
Hossein Mirzaee <hs.mirzaie@gmail.com>
2021-02-17 13:19:02 +0330

    Adding Accuracy per POS

--- Lemma.py
+++ Lemma.py
@@ -2,6 +2,8 @@ import os
 from hazm import Lemmatizer
 from parsivar import FindStems
 import re
+import matplotlib.pyplot as plt
+
 
 def extract_data(paths):
     inputs = []
@@ -14,77 +16,115 @@ def extract_data(paths):
             tokens = sentence.strip().split("\n")
             for token in tokens:
                 components = token.strip().split("\t")
-                input.append((components[1],components[3]))
+                input.append((components[1], components[3]))
                 label.append(components[2])
             inputs.append(input)
             labels.append(label)
-    return inputs,labels
+    return inputs, labels
 
 
 def Evaluate_lemmatizer(inputs, labels, lib='hazm'):
     predicted_labels_with_pos = []
     predicted_labels_no_pos = []
-
+    POS = []
     if lib == 'hazm':
         lemmatizer = Lemmatizer()
         for sentence in inputs:
             sent_labels_with_pos = []
             sent_labels_no_pos = []
+            sent_pos = []
             for (word, pos) in sentence:
                 sent_labels_with_pos.append(lemmatizer.lemmatize(word, pos))
                 sent_labels_no_pos.append(lemmatizer.lemmatize(word))
+                sent_pos.append(pos)
             predicted_labels_with_pos.append(sent_labels_with_pos)
             predicted_labels_no_pos.append(sent_labels_no_pos)
+            POS.append(sent_pos)
 
     elif lib == 'parsivar':
         stemmer = FindStems()
         for sentence in inputs:
             sent_labels_with_pos = []
             sent_labels_no_pos = []
+            sent_pos = []
             for (word, pos) in sentence:
                 sent_labels_with_pos.append(stemmer.convert_to_stem(word, pos))
                 sent_labels_no_pos.append(stemmer.convert_to_stem(word))
+                sent_pos.append(pos)
             for i in range(len(sentence)):
-                if sentence[i][1]=='V':
-                    sent_labels_with_pos[i] = re.sub(r"&",r"#",sent_labels_with_pos[i])
+                if sentence[i][1] == 'V':
+                    sent_labels_with_pos[i] = re.sub(r"&", r"#", sent_labels_with_pos[i])
                     sent_labels_no_pos[i] = re.sub(r"&", r"#", sent_labels_no_pos[i])
 
             predicted_labels_with_pos.append(sent_labels_with_pos)
             predicted_labels_no_pos.append(sent_labels_no_pos)
-
+            POS.append(sent_pos)
 
     precisions_with_pos = []
     precisions_no_pos = []
+    all_truly_labeled_with_pos = []
 
     for i in range(len(labels)):
-        truly_labeled_with_pos = sum([predicted_labels_with_pos[i][j] == labels[i][j] for j in range(len(labels[i]))])
-        truly_labeled_no_pos = sum([predicted_labels_no_pos[i][j] == labels[i][j] for j in range(len(labels[i]))])
-        precision_with_pos = truly_labeled_with_pos / len(labels[i])
-        precision_no_pos = truly_labeled_no_pos / len(labels[i])
+        truly_labeled_with_pos = [predicted_labels_with_pos[i][j] == labels[i][j] for j in range(len(labels[i]))]
+        all_truly_labeled_with_pos.append(truly_labeled_with_pos)
+        Num_truly_labeled_with_pos = sum(truly_labeled_with_pos)
+        truly_labeled_no_pos = [predicted_labels_no_pos[i][j] == labels[i][j] for j in range(len(labels[i]))]
+        Num_truly_labeled_no_pos = sum(truly_labeled_no_pos)
+
+        precision_with_pos = Num_truly_labeled_with_pos / len(labels[i])
+        precision_no_pos = Num_truly_labeled_no_pos / len(labels[i])
         precisions_with_pos.append(precision_with_pos)
         precisions_no_pos.append(precision_no_pos)
 
+    per_POS = {}
+    for i in range(len(POS)):
+        for j in range(len(POS[i])):
+            if POS[i][j] not in per_POS.keys():
+                per_POS[POS[i][j]] = {'true': 0, 'false': 0}
+            else:
+                if all_truly_labeled_with_pos[i][j]:
+                    per_POS[POS[i][j]]['true'] += 1
+                else:
+                    per_POS[POS[i][j]]['false'] += 1
+
+    accuracy_per_POS = {k: v['true'] / (v['true'] + v['false']) for k, v in per_POS.items()}
     precision_with_pos = sum(precisions_with_pos) / len(precisions_with_pos)
     precision_no_pos = sum(precisions_no_pos) / len(precisions_no_pos)
-    return (precision_with_pos,precision_no_pos)
+    return precision_with_pos, precision_no_pos, accuracy_per_POS
+
 
 Directory_Path = 'E:\\Arman\\Persian NLP toolkit\\Lemma_PerDT'
 train_Filename = 'train.conll.txt'
 dev_Filename = 'dev.conll.txt'
 test_Filename = 'test.conll.txt'
 
-paths = []
-paths.append(os.path.join(Directory_Path, train_Filename))
-paths.append(os.path.join(Directory_Path, dev_Filename))
-paths.append(os.path.join(Directory_Path, test_Filename))
-
+paths = [os.path.join(Directory_Path, train_Filename),
+         os.path.join(Directory_Path, dev_Filename),
+         os.path.join(Directory_Path, test_Filename)]
 
 inputs, labels = extract_data(paths)
 
-(parsivar_precision_with_pos, parsivar_precision_no_pos) = Evaluate_lemmatizer(inputs, labels, lib = 'parsivar')
-(hazm_precision_with_pos, hazm_precision_no_pos) = Evaluate_lemmatizer(inputs, labels, lib = 'hazm')
+(hazm_precision_with_pos, hazm_precision_no_pos, hazm_accuracy_per_POS) = \
+    Evaluate_lemmatizer(inputs, labels, lib='hazm')
+(parsivar_precision_with_pos, parsivar_precision_no_pos, parsivar_accuracy_per_POS) = \
+    Evaluate_lemmatizer(inputs, labels, lib='parsivar')
 
 print(f'hazm_precision_with_pos = {hazm_precision_with_pos}')
 print(f'parsivar_precision_with_pos = {parsivar_precision_with_pos}')
 print(f'hazm_precision_no_pos = {hazm_precision_no_pos}')
 print(f'parsivar_precision_no_pos = {parsivar_precision_no_pos}')
+
+plt.xticks(rotation='vertical')
+plt.bar(*zip(*hazm_accuracy_per_POS.items()))
+plt.title('hazm_accuracy_per_POS')
+plt.xlabel('POS')
+plt.ylabel('accuracy')
+plt.savefig(os.path.join(Directory_Path, 'hazm_accuracy_per_POS.png'), dpi=300, bbox_inches='tight')
+
+plt.figure()
+plt.xticks(rotation='vertical')
+plt.bar(*zip(*parsivar_accuracy_per_POS.items()))
+plt.title('parsivar_accuracy_per_POS')
+plt.xlabel('POS')
+plt.ylabel('accuracy')
+plt.savefig(os.path.join(Directory_Path, 'parsivar_accuracy_per_POS.png'), dpi=300, bbox_inches='tight')
